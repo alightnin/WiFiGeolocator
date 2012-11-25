93c308544f436722e2b2102b526d176c1a2fb344
jdmellor <jdmellor@localhost.(none)>
2012-11-25 16:28:25 -0700

    markers working

--- Final Project/WifiGeolocator/assets/www/js/map.js
+++ Final Project/WifiGeolocator/assets/www/js/map.js
@@ -1,4 +1,5 @@
 //Map JS
+var serviceURL = "http://jmellor.net/wardriveapp/";
 $(function() {
 	var yourStartLatLng = new google.maps.LatLng(39, -108);
 		$('#map_canvas').gmap({'center': yourStartLatLng});
@@ -9,19 +10,16 @@ $('#map').live("pageshow", function() {
 });
 $('#map').live("pageinit", function() {
 	$('#map_canvas').gmap({'center': '39, -108'});
-//	$('#map_canvas').gmap('addMarker', {'position': '39,-108', 'bounds': true}).click(function() {
-//		$('#map_canvas').gmap('openInfoWindow', {'content': 'Hello World!'}, this);
-//	});
-	//alert("hi");
-		$.getJSON( 'http://www.jmellor.net/wardriveapp/json.php?callback=?', function(data) { 
-			console.log(data);
-			$.each( data.markers, function(i, marker) {
-				console.log(marker.latitude);
+
+		$.getJSON(serviceURL+'/json.php', function(data) { 
+			$.each( data.aps, function(i, marker) {
 				$('#map_canvas').gmap('addMarker', { 
 					'position': new google.maps.LatLng(marker.latitude, marker.longitude), 
 					'bounds': true 
 				}).click(function() {
-					$('#map_canvas').gmap('openInfoWindow', { 'content': marker.content }, this);
+					var str="<p style=\"color:black;\"><b>" + marker.ssid + "</b> <br/> Mac Address: <i>" + marker.mac + "</i><br/> Security: <i>" + marker.security + "</i><br/> Channel: <i>" + marker.channel + "</i><br/>Signal Strength: <i>" + marker.signal + "</i></p>";
+
+					$('#map_canvas').gmap('openInfoWindow', { 'content': str }, this);
 				});
 			});
 		});
